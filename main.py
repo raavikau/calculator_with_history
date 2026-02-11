9070de96d7094f35c1523b4c4d262ad420299d9f
Raavi Kaur <raavi211299@gmail.com>
2026-02-10 20:51:57 -0800

    add the basic calculation code in main file

---
+++ main.py
@@ -0,0 +1,17 @@
+def calculation(a, b, operator):
+    if operator == "+":
+        return a + b
+    elif operator == "-":
+        return a - b
+    elif operator == "*":
+        return a * b
+    elif operator == "/":
+        return a / b
+    else:
+        print("wrong operator")
+
+first = int(input("enter the first value: "))
+second = int(input("enter the second value: "))
+choice = input("please check +, -, *, /")
+
+print(calculation(first, second, choice))
