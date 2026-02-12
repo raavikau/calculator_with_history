5d4751956f361fbb33686ccdad317ebff156ea95
Raavi Kaur <raavi211299@gmail.com>
2026-02-12 15:41:59 -0800

    add the other operations you want to perform

--- main.py
+++ main.py
@@ -15,6 +15,16 @@ def calculation(a, b, operator):
 
 first = int(input("enter the first value: "))
 second = int(input("enter the second value: "))
-choice = input("please check +, -, *, /")
 
-print(calculation(first, second, choice))
+choice = input("Enter +, -, *, / or type history, clear, quit ")
+if choice == "quit":
+    pass
+elif choice == "clear":
+    pass
+elif choice == "history":
+    pass
+elif choice in ["+", "-", "*", "/"]:
+    result = calculation(first, second, choice)
+    print(result)
+else:
+    print("Invalid command")
