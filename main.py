d7def7789264f73b56d6ee33b6902e52d4857439
Raavi Kaur <raavi211299@gmail.com>
2026-02-23 22:00:30 -0800

    save the operation to the text file

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 def calculation(a, b, operator):
     if operator == "+":
-        return a + b
+        return f"{a} + {b} = {a + b} \n"
     elif operator == "-":
-        return a - b
+        return f"{a} - {b} = {a - b} \n"
     elif operator == "*":
-        return a * b
+        return f"{a} * {b} = {a * b} \n"
     elif operator == "/":
         if b == 0:
             print("Error: division by zero")
             return None
-        return a / b
+        return f"{a} / {b} = {a / b} \n"
     else:
         print("wrong operator")
 
 def save_history():
     try:
-        with open("history.txt", 'r') as readfile:
+        with open("history.txt", "r") as readfile:
             content = readfile.read()
             if content == "":
                 print("file is empty")
@@ -23,9 +23,15 @@ def save_history():
     except FileNotFoundError:
         print("No history file found")
 
+def calculator(c, d, operation):
+    result = calculation(c, d, operation)
+    if result is not None:
+        with open("history.txt", "a") as file:
+            file.write(result)
+        print("Successfully added")
+
 def clear_history():
     pass
-
 
 while True:
     choice = input("Enter +, -, *, / or type history, clear, quit ")
@@ -39,7 +45,6 @@ while True:
     elif choice in ["+", "-", "*", "/"]:
         first = int(input("enter the first value: "))
         second = int(input("enter the second value: "))
-        result = calculation(first, second, choice)
-        print(result)
+        calculator(first, second, choice)
     else:
         print("Invalid command")
